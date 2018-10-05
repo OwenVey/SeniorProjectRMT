60bccde34c2a38125ae4bc86fdff04e9799be63d
Owen Vey <owen.vey@gmail.com>
2018-10-04 23:18:13 -0500

    added welcome message to home screen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar/Navbar.jsx'
+import { Container, Header } from 'semantic-ui-react'
 
 class App extends Component {
+
+
+
   render() {
     return (
       <div className='App'>
+
         <Navbar></Navbar>
+
+        <div className='center'>
+          <Header
+            as='h1'
+            content='Welcome!'
+            style={{
+              fontSize: '4em',
+              fontWeight: 'normal',
+            }}
+          />
+        </div>
+
       </div>
     );
   }
