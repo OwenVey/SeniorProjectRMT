0cd17fdd5d89dac696ed6a7aa733ab7bd6a61029
Owen Vey <owen.vey@gmail.com>
2018-10-07 12:19:20 -0500

    cleanup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar/Navbar.jsx'
-import { Container, Header } from 'semantic-ui-react'
 
 class App extends Component {
-
-
-
   render() {
     return (
       <div className='App'>
 
         <Navbar></Navbar>
-
-        
 
       </div>
     );
