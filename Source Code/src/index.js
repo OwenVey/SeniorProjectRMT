d9d61fd1dfa390f70ad3f0ad02b289381a749cc2
Rhett Krzykowski <rjkrzykowski@yahoo.com>
2018-10-11 17:45:16 -0500

    Created Project Dashboard Page. Added label for Document and Expanded View. Added navigation from NavBar for testing

--- Source Code/src/index.js
+++ Source Code/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './components/App/App.jsx';
 import LoginForm from './components/LoginForm/LoginForm.jsx';
+import ProjectDashboard from './components/ProjectDashboard/ProjectDashboard.jsx'
 import SignUpForm from './components/SignUpForm/SignUpForm.jsx';
 import Welcome from './components/Welcome/Welcome.jsx';
 
@@ -17,6 +18,7 @@ ReactDOM.render(
       <Route path='/' component={App} />
       <Route exact path='/home' component={Welcome} />
       <Route exact path='/login' component={LoginForm} />
+      <Route exact path='/projectdashboard' component={ProjectDashboard} />
       <Route exact path='/signup' component={SignUpForm} />
     </div>
   </Router>
