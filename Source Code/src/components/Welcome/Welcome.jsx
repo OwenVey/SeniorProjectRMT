3a2a7132b55c601f1254c03852c863a041a57b02
Owen Vey <owen.vey@gmail.com>
2018-10-11 18:00:32 -0500

    added list options and select animations

--- Source Code/src/components/Welcome/Welcome.jsx
+++ Source Code/src/components/Welcome/Welcome.jsx
@@ -1,28 +1,67 @@
 import React from 'react'
-import { Header, Menu, Container, Button } from 'semantic-ui-react'
+import { Header, Menu, Container, Button, List } from 'semantic-ui-react'
 
 const Welcome = () => (
 
   <div>
     <div className='welcome'>
-    <Header
-      as='h1'
-      content='Welcome!'
-      style={{
-        fontSize: '4em',
-        fontWeight: 'normal',
-      }}
-    />
+      <Header
+        as='h1'
+        content='Welcome!'
+        style={{
+          fontSize: '4em',
+          fontWeight: 'normal',
+        }}
+      />
     </div>
+
     <Container>
-      <Menu tabular>
-          <Menu.Item name='bio' active/>
-          <Menu.Menu position='right'>
-            <Button color='teal'>Hello</Button>
-            </Menu.Menu>
-        </Menu>
-        </Container>
-  </div>
+      <Menu pointing secondary size='massive'>
+        <Menu.Item active name='Projects' />
+        <Menu.Menu position='right'>
+          <Button color='teal'>New Project</Button>
+        </Menu.Menu>
+      </Menu>
+
+      <List selection animated divided relaxed size='large'>
+
+
+
+        <List.Item onClick={function () { alert('hello') }}>
+          <List.Content floated='right' verticalAlign='middle'>
+            1.0
+          </List.Content>
+          <List.Content>
+            <List.Header>Project 1</List.Header>
+            This is the description for Project 1
+
+          </List.Content>
+        </List.Item>
+
+        <List.Item onClick={function () { alert('hello') }}>
+          <List.Content floated='right' verticalAlign='middle'>
+            1.0
+          </List.Content>
+          <List.Content>
+            <List.Header>Project 2</List.Header>
+            This is the description for Project 2
+          </List.Content>
+        </List.Item>
+
+        <List.Item onClick={function () { alert('hello') }}>
+          <List.Content floated='right'>
+            1.0
+          </List.Content>
+          <List.Content>
+            <List.Header>Project 3</List.Header>
+            This is the description for Project 3
+          </List.Content>
+        </List.Item>
+
+      </List>
+
+    </Container>
+  </div >
 )
 
 export default Welcome
