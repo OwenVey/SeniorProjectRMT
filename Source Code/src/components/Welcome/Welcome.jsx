14d63e98650287295f482b821e384550fa774c89
Samantha Murphy <softwarecoderengr@gmail.com>
2018-10-11 17:01:51 -0500

    Added Button for Projects as well as a button to create new projects, need to e renamed and formatted

--- Source Code/src/components/Welcome/Welcome.jsx
+++ Source Code/src/components/Welcome/Welcome.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Header } from 'semantic-ui-react'
+import { Header, Menu, Container, Button } from 'semantic-ui-react'
 
 const Welcome = () => (
 
+  <div>
     <div className='welcome'>
     <Header
       as='h1'
@@ -12,8 +13,16 @@ const Welcome = () => (
         fontWeight: 'normal',
       }}
     />
+    </div>
+    <Container>
+      <Menu tabular>
+          <Menu.Item name='bio' active/>
+          <Menu.Menu position='right'>
+            <Button color='teal'>Hello</Button>
+            </Menu.Menu>
+        </Menu>
+        </Container>
   </div>
-
 )
 
 export default Welcome
