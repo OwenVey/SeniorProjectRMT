2d13beabf6cc3e35e676760318f2d9a4f420f082
Owen Vey <owen.vey@gmail.com>
2018-11-06 00:28:06 -0600

    fixed bug with navbar not showing correct page after refresh

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './Navbar.css'
+import './Navbar.css';
 
 const { Header } = Layout;
 
 class Navbar extends Component {
 
-
   render() {
+    let href = window.location.pathname;
     if (window.location.pathname === '/login') return null;
     return (
       <Header style={{ justifyContent: 'center' }} className='header'>
@@ -23,25 +23,25 @@ class Navbar extends Component {
           className='menu'
           theme='dark'
           mode='horizontal'
-          defaultSelectedKeys={['1']}
+          selectedKeys={[href]}
           style={{ lineHeight: '50px' }}
         >
 
-          <Menu.Item className='menu-item' key='1'>
+          <Menu.Item className='menu-item' key='/home'>
             <NavLink to='/home'>
               <FontAwesomeIcon className='navbar-icon' icon='home' />
               Home
           </NavLink>
           </Menu.Item>
 
-          <Menu.Item className='menu-item' key='2'>
+          <Menu.Item className='menu-item' key='/project'>
             <NavLink to='/project'>
               <FontAwesomeIcon className='navbar-icon' icon='archive' />
               Projects
           </NavLink>
           </Menu.Item>
 
-          <Menu.Item className='menu-item' key='3'>
+          <Menu.Item className='menu-item' key='/admin'>
             <NavLink to='/admin'>
               <FontAwesomeIcon className='navbar-icon' icon='lock' />
               Admin
@@ -49,14 +49,14 @@ class Navbar extends Component {
           </Menu.Item>
 
 
-          <Menu.Item className='menu-item' key='5' onClick={this.props.onLogout} style={{ float: 'right', marginRight: '75px' }}>
+          <Menu.Item className='menu-item' key='/logout' onClick={this.props.onLogout} style={{ float: 'right', marginRight: '75px' }}>
             <NavLink to='/login'>
               <FontAwesomeIcon className='navbar-icon' icon='sign-out-alt' />
               Log Out
           </NavLink>
           </Menu.Item>
 
-          <Menu.Item className='menu-item' key='4' style={{ float: 'right' }}>
+          <Menu.Item className='menu-item' key='/user' style={{ float: 'right' }}>
             <NavLink to='/user'>
               <FontAwesomeIcon className='navbar-icon' icon='user' />
               Your Name
