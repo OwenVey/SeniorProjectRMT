0114fca466b0f6835601b991cc32c1d1d13a02cf
Alex Tilot <tilota@uwplatt.edu>
2018-11-07 16:25:56 -0600

    Added table for user's

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,57 +1,74 @@
-import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import Navbar from '../Navbar/Navbar.jsx';
-import PrivateRoute from '../PrivateRoute/PrivateRoute.jsx';
-import LoginPage from '../LoginPage/LoginPage.jsx';
-import HomePage from '../Home/HomePage/HomePage.jsx';
-import ProjectPage from '../Projects/ProjectPage/ProjectPage.jsx';
-import AdminPage from '../Admin/AdminPage/AdminPage.jsx';
-import PageNotFound from '../PageNotFound/PageNotFound.jsx';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import Navbar from "../Navbar/Navbar.jsx";
+import PrivateRoute from "../PrivateRoute/PrivateRoute.jsx";
+import LoginPage from "../LoginPage/LoginPage.jsx";
+import HomePage from "../Home/HomePage/HomePage.jsx";
+import ProjectPage from "../Projects/ProjectPage/ProjectPage.jsx";
+import AdminPage from "../Admin/AdminPage/AdminPage.jsx";
+import PageNotFound from "../PageNotFound/PageNotFound.jsx";
 
 library.add(fas, far);
 
 class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
-      isAuthenticated: false,
+      isAuthenticated: true
     };
   }
 
   login = () => {
     this.setState({
       isAuthenticated: true
-    })
-  }
+    });
+  };
 
   logout = () => {
     this.setState({
       isAuthenticated: false
-    })
-  }
+    });
+  };
 
-  handleProjectAdded = (project) => {
+  handleProjectAdded = project => {
     this.setState(prevState => ({
       projects: [...prevState.projects, project]
-    }))
-  }
+    }));
+  };
 
   render() {
     return (
-      <div className='app'>
-        <Navbar onLogout={this.logout}></Navbar>
+      <div className="app">
+        <Navbar onLogout={this.logout} />
         <Switch>
-          <Route path='/login' render={props => <LoginPage {...props} onLogin={this.login} />} />
+          <Route
+            path="/login"
+            render={props => <LoginPage {...props} onLogin={this.login} />}
+          />
           <Route exact path="/" render={() => <Redirect to="/home" />} />
-          <PrivateRoute authed={this.state.isAuthenticated} path='/home' component={HomePage} />
-          <PrivateRoute authed={this.state.isAuthenticated} path='/project' component={ProjectPage} />
-          <PrivateRoute authed={this.state.isAuthenticated} path='/admin' component={AdminPage} />
-          <PrivateRoute authed={this.state.isAuthenticated} component={PageNotFound} />
+          <PrivateRoute
+            authed={this.state.isAuthenticated}
+            path="/home"
+            component={HomePage}
+          />
+          <PrivateRoute
+            authed={this.state.isAuthenticated}
+            path="/project"
+            component={ProjectPage}
+          />
+          <PrivateRoute
+            authed={this.state.isAuthenticated}
+            path="/admin"
+            component={AdminPage}
+          />
+          <PrivateRoute
+            authed={this.state.isAuthenticated}
+            component={PageNotFound}
+          />
         </Switch>
       </div>
     );
