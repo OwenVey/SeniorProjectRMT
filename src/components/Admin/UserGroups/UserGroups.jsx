3ac534b67d72f4d731b4879142908ad87d5c4498
Samantha Murphy <softwarecoderengr@gmail.com>
2018-11-14 16:40:05 -0600

    Created the Add, Edit, and Remove buttons for the User Groups part of the Admin page. Began work on the modal for adding a User Group.

--- src/components/Admin/UserGroups/UserGroups.jsx
+++ src/components/Admin/UserGroups/UserGroups.jsx
@@ -1,10 +1,113 @@
 import React, { Component } from 'react';
-import { Tag, Table, Divider } from 'antd';
+import { Tag, Table, Divider, Modal, Row, Button, Radio, Icon, Alert, Input, Col, Select, InputNumber, DatePicker, AutoComplete, Cascader, Transfer } from 'antd';
+import "./UserGroups.css"
+
+const InputGroup = Input.Group;
+const Option = Select.Option;
 
 class UserGroups extends Component {
+  state = {
+    size: 'large',
+    mockData: [],
+    targetKeys: [],
+  };
+  edit = e => {
+    alert('Edit User Group button has been clicked');
+  }
+  delete = e => {
+    alert('Delete User Group button has been clicked');
+  }
+
+  addModal = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+  componentDidMount() {
+    this.getMock();
+  }
+
+  getMock = () => {
+    const targetKeys = [];
+    const mockData = [];
+    for (let i = 0; i < 20; i++) {
+      const data = {
+        key: i.toString(),
+        title: `content${i + 1}`,
+        description: `description of content${i + 1}`,
+        chosen: Math.random() * 2 > 1,
+      };
+      if (data.chosen) {
+        targetKeys.push(data.key);
+      }
+      mockData.push(data);
+    }
+    this.setState({ mockData, targetKeys });
+  }
+
+  filterOption = (inputValue, option) => {
+    return option.description.indexOf(inputValue) > -1;
+  }
+
+  handleChange = (targetKeys) => {
+    this.setState({ targetKeys });
+  }
+
   render() {
+    const size = this.state.size;
+    
     return (
+      <div>
+        <Radio.Group value={size}>
+          <Radio.Button value="default" onClick={this.addModal}>Add New User Group</Radio.Button>
+          <Radio.Button value="default" onClick={this.edit}>Edit Selected User Group</Radio.Button>
+          <Radio.Button value="default" onClick={this.delete}>Delete User Group</Radio.Button>
+        </Radio.Group>
+        <Modal
+          title="Add User Group"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          className="userGroupModal">
+          <Row className="inputRow">
+          <Input 
+            title="userGroupTitle"
+            placeholder="New User Group Title"
+            ></Input></Row>
+          <Row className="inputRow"><Input 
+            title="userGroupDescription"
+            placeholder="New User Group Description"></Input></Row>
+          <Divider />
+          <Transfer
+        dataSource={this.state.mockData}
+        showSearch
+        filterOption={this.filterOption}
+        targetKeys={this.state.targetKeys}
+        onChange={this.handleChange}
+        render={item => item.title}
+        listStyle={{
+          width: 200,
+          height: 300,
+        }}
+        className="transferUsers"
+      />
+        </Modal>
       <Table bordered dataSource={data} columns={columns} />
+      </div>
     )
   }
 }
