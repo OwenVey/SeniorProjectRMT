3747e9c0a3aca6bb63b00d08a649c81d16146279
Zachary A Pollard <pollardz@uwplatt.edu>
2018-11-27 16:17:19 -0600

    edit user for admin page

--- src/components/Admin/Users/Users.jsx
+++ src/components/Admin/Users/Users.jsx
@@ -1,47 +1,73 @@
 import React, { Component } from 'react';
+import {Link } from 'react-router-dom';
 import ReactDragListView from 'react-drag-listview';
-import { Table, Tag, Divider } from 'antd';
+import { Table, Tag, Modal, Button, Radio, Input } from 'antd';
 
 class Users extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      loading: false, 
+      visible: false,
+      index: 0,
+      name: "FirstName LastName",
+      title:  'FirstName LastName - username',
+      email: 'email@email.com',
+      userName: 'username',
+      userGroups: 'UserGroups',
+      liscenceType: 'liscenceType',
+      userStatus: 'ACTIVE',
+
       data: [
         {
-          fullName: 'Alex Tilot',
+          index: 0,
+          name: 'Alex Tilot',
+          fullName:  <Link to='/admin' onClick={this.setAlex}> Alex Tilot </Link>,
           email: 'alextilot@gmail.com',
           userName: 'Nezzely',
+          password: '1234',
           userGroups: ['Purple', 'Blue', 'Brown'],
           liscenceType: 'Developer',
           userStatus: 'ACTIVE',
           actions: ''
         },
         {
-          fullName: 'Jared Bloomfield',
+          index: 1,
+          name: 'Jared Bloomfield',
+          fullName: <Link to='/admin'onClick={this.setJared}> Jared Bloomfield </Link>,
+          
           email: 'Jaredbloomfield@gmail.com',
           userName: 'Jrod744',
+          password: '1234',
           userGroups: ['Red', 'White', 'Yellow'],
           liscenceType: 'Developer',
           userStatus: 'ACTIVE',
           actions: ''
+          
         },
         {
-          fullName: 'Owen Vey',
+          index: 2,
+          name: 'Owen Vey',
+          fullName: <Link to='/admin'onClick={this.setOwen}> Owen Vey </Link>,
           email: 'owenvey@gmail.com',
           userName: 'Slopeur',
+          password: '1234',
           userGroups: ['Black', 'Pink', 'Silver'],
           liscenceType: 'PO',
           userStatus: 'ACTIVE',
           actions: ''
         },
         {
-          fullName: 'Josh Debaets',
+          index: 3,
+          name: 'Josh Debaets',
+          fullName: <Link to='/admin'onClick={this.setJosh}> Josh Debaets </Link>,
           email: 'joshdebaets@gmail.com',
           userName: 'Debaets',
+          password: '1234',
           userGroups: ['Green', 'Orange', 'Cyan'],
           liscenceType: 'Developer',
-          userStatus: 'DEACTIVE',
+          userStatus: 'INACTIVE',
           actions: ''
         }
       ],
@@ -51,7 +77,7 @@ class Users extends Component {
           dataIndex: 'fullName',
           key: 'fullname',
           defaultSortOrder: 'ascend',
-          sorter: (a, b) => a.fullName.localeCompare(b.fullName)
+          sorter: (a, b) => a.name.localeCompare(b.fullName)
         },
         {
           title: 'User Name',
@@ -92,31 +118,75 @@ class Users extends Component {
           key: 'userStatus',
           sorter: (a, b) => a.userStatus.localeCompare(b.userStatus)
         },
-        {
-          //Actions-> edit, password, subscriptions, invite deactivate
-          title: 'Actions',
-          key: 'actions',
-          render: () => (
-            <span>
-              <a href=''>Edit</a>
-              <Divider type='vertical' />
-              <a href=''>Password</a>
-              <Divider type='vertical' />
-              <a href=''>Subscriptions</a>
-              <Divider type='vertical' />
-              <a href=''>Invite</a>
-              <Divider type='vertical' />
-              <a href=''>Deactivate</a>
-            </span>
-          ),
+        // {
+        //   //Actions-> edit, password, subscriptions, invite deactivate
+        //   title: 'Actions' ,
+        //   key: 'actions',
+        //   render: () => (
+        //     <span>
+        //       <a href=''>Edit</a>
+        //       <Divider type='vertical' />
+        //       <a href=''>Password</a>
+        //       <Divider type='vertical' />
+        //       <a href=''>Subscriptions</a>
+        //       <Divider type='vertical' />
+        //       <a href=''>Invite</a>
+        //       <Divider type='vertical' />
+        //       <a href=''>Deactivate</a>
+        //     </span>
+        //   ),
 
-        }
+        // }
       ]
     };
+
+
   }
+  setAlex = () => this.setState( { index: 0 },
+  this.showModal
+  );
+
+  setJared = () => this.setState( { index: 1 },
+  this.showModal
+  );
+  setOwen = () => this.setState( { index: 2},
+  this.showModal
+  );
+  setJosh = () => this.setState( { index: 3},
+  this.showModal
+  );
+
+  showModal = () => this.setState( { visible: true });
+
+    handleOk = () => {
+      this.setState({ loading: true })
+      setTimeout(() => {
+        this.setState({ loading: false, visible: false });
+      }, 3000);
+    }
+
+    handleCancel = () => { this.setState({ visible: false })
+    ;}
+
+    onChangeUserName = (e) => {
+      const {index} = this.state;
+      this.state.data[index].userName = e.target.value };
+    onChangeName = (e) => {
+      const {index} = this.state;
+      this.state.data[index].name = e.target.value };
+    onChangeEmail = (e) => {
+      const {index} = this.state;
+      this.state.data[index].email = e.target.value };
+    onChangePassword = (e) => {
+      const {index} = this.state;
+      this.state.data[index].password = e.target.value };
+      
 
   render() {
     const that = this;
+
+    const { visible, loading, userStatus, index} = this.state;
+
     this.dragProps = {
       onDragEnd(fromIndex, toIndex) {
         const columns = that.state.columns;
@@ -139,11 +209,36 @@ class Users extends Component {
             bordered
           />
         </ReactDragListView.DragColumn>
+
+        <Modal
+          visible={visible}
+          title= {this.state.data[index].username}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          userStatus = {userStatus}
+          footer={[
+            <Button key="back" onClick={this.handleCancel}> Cancel</Button>,
+            <Button key="update" type="primary" loading={loading} onClick={this.handleOk}>
+              Update
+            </Button>,
+          ]}
+        >
+          <p> Name: <Input  deafultValue = "" placeholder= {this.state.data[index].name} onChange={this.onChangeName} /> </p>
+          <p> Password: <Input placeholder= {this.state.data[index].password} onChange={this.onChangePassword}/> </p>
+          <p> Email: <Input placeholder= {this.state.data[index].email} onChange={this.onChangeEmail} /></p>
+          <p> Username: <Input placeholder= {this.state.data[index].userName} onChange={this.onChangeUserName}/> </p>
+          <p> Current Status: {this.state.data[index].userStatus} </p>
+          <Radio.Group  buttonStyle="solid" >
+          <Radio.Button  onChange={this.Ina} value = 'ACTIVE'  > ACTIVE </Radio.Button>
+          <Radio.Button  value = 'INACTIVE' > INACTIVE </Radio.Button>
+        </Radio.Group>
+        </Modal>
       </div>
+      
     );
   }
 }
-//Bar: ViewInactie users, AddUser, Search ->ClearFunction
+//Bar: ViewInactive users, AddUser, Search ->ClearFunction
 //Users: UserName,FullName, Email, LoginName, UserGroups, LiscenceType, UserStatus,
 //Actions-> edit, password, subscriptions, invite deactivate
 export default Users;
