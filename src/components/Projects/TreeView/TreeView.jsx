006cfd8096f98ecd97b8829b881b557f752ba8f7
Samantha Murphy <softwarecoderengr@gmail.com>
2019-02-09 15:02:24 -0600

    Comments

--- src/components/Projects/TreeView/TreeView.jsx
+++ src/components/Projects/TreeView/TreeView.jsx
@@ -193,6 +193,8 @@ class TreeView extends Component {
     }
   }
 
+  /* This ensures that fetchTree is called.
+  */
   componentWillMount() {
     this.fetchTree();
   }
@@ -241,7 +243,6 @@ class TreeView extends Component {
   }
 
   render() {
-
     const rightClickTreeNodeMenu = (
       <Menu>
         <Menu.Item key="1">Open</Menu.Item>
