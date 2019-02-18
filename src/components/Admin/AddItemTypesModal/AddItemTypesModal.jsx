abd0e607a0efdbbf955df8284a77526fb8a7a5e5
Samantha Murphy <softwarecoderengr@gmail.com>
2019-02-18 10:51:01 -0600

    Fixes for AddItemTypes

---
+++ src/components/Admin/AddItemTypesModal/AddItemTypesModal.jsx
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+import { Icon, Modal, Input, Select, Form, DatePicker } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+
+const Option = Select.Option;
+const FormItem = Form.Item;
+
+class AddItemTypesModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            confirmDirty: false,
+        }
+    }
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+
+    handleCancel = (e) => {
+        this.setState({
+            visible: false,
+        });
+    }
+
+    handleAddItemType = (e) => {
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                const { icon, display,/* plural, key,*/ description, id, system } = this.state;
+                let newItemType = {
+                    icon,
+                    display,
+                    //plural,
+                    //key,
+                    description,
+                    id,
+                    system,
+                }
+                this.setState({
+                    visible: false,
+                    itemTypes: [...this.state.itemTypes, newItemType],
+
+                });
+            }
+        })
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 40 },
+                sm: { span: 40 },
+            },
+        };
+        return (
+            <div>
+                <div style={{ display: 'flex', flexDirection: 'row', margin: 20, alignItems: 'center', justifyContent: 'space-between' }}>
+                    <h2 style={{ marginBottom: 0 }}>Item Types</h2>
+                    <Button id="button" onClick={this.showModal} style={{ marginRight: 0 }}>
+                        <Icon type="plus-circle" theme='filled' style={{ color: "#1890ff" }}></Icon>Add item type
+          </Button>
+                </div>
+                <Modal
+                    title={<div><Icon type='bars' style={{ color: '#1890ff' }}></Icon> Add Item Types</div>}
+                    visible={this.state.visible}
+                    onOk={this.handleAddItemType} //Needed so that hitting the Add button works
+                    onCancel={this.handleCancel}
+                    okText="Add Item Type"
+                >
+                    <Form onOk={this.handleAddItemType}>
+                        <FormItem style={{ marginBottom: '0px' }} label="Icon">
+                            {getFieldDecorator('iconName', {
+                                rules: [
+                                    { required: true, message: 'Please input icon name' },
+                                    { max: 255, message: 'Name must be 255 characters or less' }],
+                            })
+                                (<Select
+                                    className="inputFields"
+                                    labelInValue
+                                    style={{ width: 200 }}
+                                    onChange={this.handleChange}
+                                    placeholder='Select icon'
+                                >
+                                    <Option value='projects'><Icon><FontAwesomeIcon icon="archive" /></Icon></Option>
+                                    <Option value="attachments"><Icon><FontAwesomeIcon icon="paperclip" /></Icon></Option>
+                                    <Option value="requirements"><Icon><FontAwesomeIcon icon="file-signature" /></Icon></Option>
+                                    <Option value="note"><Icon><FontAwesomeIcon icon="file-alt" /></Icon></Option>
+                                </Select>)}
+                        </FormItem>
+                        <FormItem style={{ marginBottom: '0px' }} label="Display">
+                            {getFieldDecorator('name', {
+                                rules: [
+                                    { required: true, message: 'Please input item type\'s name' },
+                                    { max: 255, message: 'Name must be 255 characters or less' }],
+                            })
+                                (<Input required={true} className="inputFields" value={this.state.display} onChange={(e) => this.setState({ display: e.target.value })} placeholder='Display' />)}
+                        </FormItem>
+
+                        {/* <div className="labels">Plurals</div>
+              <Input className="inputFields" value={this.state.plural} onChange={(e) => this.setState({ plural: e.target.value })} placeholder="Plural" />
+              <div className="labels">Key</div>
+              <Input className="inputFields" value={this.state.key} onChange={(e) => this.setState({ key: e.target.value })} placeholder="Key" />*/}
+
+                        <FormItem style={{ marginBottom: '0px' }} label="Description">
+                            {getFieldDecorator('name', {
+                                rules: [
+                                    { required: true, message: 'Please input item type\'s description' },
+                                    { max: 255, message: 'Name must be 255 characters or less' }],
+                            })
+                                (<Input.TextArea className="inputFields" value={this.state.description} onChange={(e) => this.setState({ description: e.target.value })} placeholder="Description" />)}
+                        </FormItem>
+                        <FormItem style={{ marginBottom: '0px' }} label="Id">
+                            {getFieldDecorator('id', {
+                                rules: [
+                                    { required: true, message: 'Please input item type\'s id' },
+                                    { max: 255, message: 'Name must be 255 characters or less' }],
+                            })
+                                (<Input className="inputFields" value={this.state.id} onChange={(e) => this.setState({ id: e.target.value })} placeholder="id" />)}
+                        </FormItem>
+                        <FormItem style={{ marginBottom: '0px' }} label="ProjectID">
+                            {getFieldDecorator('ProjectId', {
+                                rules: [
+                                    { required: true, message: 'Please input item type\'s ProjectIDs' },
+                                    { max: 255, message: 'Name must be 255 characters or less' }],
+                            })
+                                (<Input className="inputFields" value={this.state.id} onChange={(e) => this.setState({ projectId: e.target.value })} placeholder="id" />)}
+                        </FormItem>
+                    </Form>
+                </Modal>
+                <div style={{ margin: 20 }}>
+                    <Table
+                        columns={this.state.columns}
+                        pagination={false}
+                        dataSource={this.state.itemTypes}
+                        icon={<FontAwesomeIcon />}
+                        bordered
+                    />
+                </div>
+            </div >
+        );
+    }
+
+}
+
+export default Form.create()(AddItemTypesModal);
--- src/components/Admin/ItemTypes/ItemTypes.jsx
+++ src/components/Admin/ItemTypes/ItemTypes.jsx
@@ -3,6 +3,7 @@ import { Divider, Table, Button, Modal, Input, Icon, Form } from "antd";
 import { Select } from "antd";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './ItemTypes.css';
+
 const { Option } = Select;
 const FormItem = Form.Item;
 
@@ -226,76 +227,9 @@ class ItemTypes extends Component {
         <div style={{ display: 'flex', flexDirection: 'row', margin: 20, alignItems: 'center', justifyContent: 'space-between' }}>
           <h2 style={{ marginBottom: 0 }}>Item Types</h2>
           <Button id="button" onClick={this.showModal} style={{ marginRight: 0 }}>
-            <Icon type="plus-circle" theme='filled' style={{ color: "#1890ff" }}></Icon>Add item type
+            <Icon type="plus-circle" theme='filled' style={{ color: "#1890ff" }}></Icon>ADD ITEM
         </Button>
         </div>
-        <Modal
-          title={<div><Icon type='bars' style={{ color: '#1890ff' }}></Icon> Add Item Types</div>}
-          visible={this.state.visible}
-          onOk={this.handleAddItemType} //Needed so that hitting the Add button works
-          onCancel={this.handleCancel}
-          okText="Add Item Type"
-        >
-          <Form onOk={this.handleAddItemType}>
-            <FormItem style={{ marginBottom: '0px' }} label="Icon">
-              {getFieldDecorator('iconName', {
-                rules: [
-                  { required: true, message: 'Please input icon name' },
-                  { max: 255, message: 'Name must be 255 characters or less' }],
-              })
-                (<Select
-                  className="inputFields"
-                  labelInValue
-                  style={{ width: 200 }}
-                  onChange={this.handleChange}
-                  placeholder='Select icon'
-                >
-                  <Option value='projects'><Icon><FontAwesomeIcon icon="archive" /></Icon></Option>
-                  <Option value="attachments"><Icon><FontAwesomeIcon icon="paperclip" /></Icon></Option>
-                  <Option value="requirements"><Icon><FontAwesomeIcon icon="file-signature" /></Icon></Option>
-                  <Option value="note"><Icon><FontAwesomeIcon icon="file-alt" /></Icon></Option>
-                </Select>)}
-            </FormItem>
-            <FormItem style={{ marginBottom: '0px' }} label="Display">
-              {getFieldDecorator('name', {
-                rules: [
-                  { required: true, message: 'Please input item type\'s name' },
-                  { max: 255, message: 'Name must be 255 characters or less' }],
-              })
-                (<Input required={true} className="inputFields" value={this.state.display} onChange={(e) => this.setState({ display: e.target.value })} placeholder='Display' />)}
-            </FormItem>
-
-            {/* <div className="labels">Plurals</div>
-            <Input className="inputFields" value={this.state.plural} onChange={(e) => this.setState({ plural: e.target.value })} placeholder="Plural" />
-            <div className="labels">Key</div>
-            <Input className="inputFields" value={this.state.key} onChange={(e) => this.setState({ key: e.target.value })} placeholder="Key" />*/}
-
-            <FormItem style={{ marginBottom: '0px' }} label="Description">
-              {getFieldDecorator('name', {
-                rules: [
-                  { required: true, message: 'Please input item type\'s description' },
-                  { max: 255, message: 'Name must be 255 characters or less' }],
-              })
-                (<Input.TextArea className="inputFields" value={this.state.description} onChange={(e) => this.setState({ description: e.target.value })} placeholder="Description" />)}
-            </FormItem>
-            <FormItem style={{ marginBottom: '0px' }} label="Id">
-              {getFieldDecorator('id', {
-                rules: [
-                  { required: true, message: 'Please input item type\'s id' },
-                  { max: 255, message: 'Name must be 255 characters or less' }],
-              })
-                (<Input className="inputFields" value={this.state.id} onChange={(e) => this.setState({ id: e.target.value })} placeholder="id" />)}
-            </FormItem>
-            <FormItem style={{ marginBottom: '0px' }} label="ProjectID">
-              {getFieldDecorator('ProjectId', {
-                rules: [
-                  { required: true, message: 'Please input item type\'s ProjectIDs' },
-                  { max: 255, message: 'Name must be 255 characters or less' }],
-              })
-                (<Input className="inputFields" value={this.state.id} onChange={(e) => this.setState({ projectId: e.target.value })} placeholder="id" />)}
-            </FormItem>
-          </Form>
-        </Modal>
         <div style={{ margin: 20 }}>
           <Table
             columns={this.state.columns}
