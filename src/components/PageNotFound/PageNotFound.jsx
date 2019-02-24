80def8a312a05301a6c7a9696feb48684a6f9254
Samantha Murphy <softwarecoderengr@gmail.com>
2019-02-24 12:21:00 -0600

    cleared profile page

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -5,6 +5,8 @@ const PageNotFound = () => (
     <h1>User First Name:</h1>
     <h1>User Last Name:</h1>
     <h1>User Email:</h1>
+    <h2>Button to modify user password</h2>
+    <h2>Pencil icon to modify user information</h2>
   </div>
 );
 
