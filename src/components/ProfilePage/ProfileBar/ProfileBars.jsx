c6c25a4795f895983eaccf6213f5d471a2aa7179
Tanner <tanrh3@gmail.com>
2019-02-24 21:23:16 -0600

    Added ProfileBars.jsx

--- src/components/ProfilePage/ProfileBar/ProfileBars.jsx
+++ src/components/ProfilePage/ProfileBar/ProfileBars.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Icon, Modal, Input, Select, } from 'antd';
 import ProfilePage from './ProfilePage.jsx';
+
 export class ProfilePage extends Component {
 
 }
