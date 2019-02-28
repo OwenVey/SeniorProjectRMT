09c72a07312dc7be6f965130801976bbf627f24f
Rhett J. Krzykowski <krzykowskir@uwplatt.edu>
2019-02-28 17:27:34 -0600

    Headers Added to all current admin tabs

--- src/components/Admin/AdminBars/AdminBars.jsx
+++ src/components/Admin/AdminBars/AdminBars.jsx
@@ -4,53 +4,6 @@ import AddUserModal from '../AddUserModal/AddUserModal';
 import AddProjectModal from '../AddProjectModal/AddProjectModal';
 import AddItemTypesModal from '../AddItemTypesModal/AddItemTypesModal';
 const Option = Select.Option;
-//#region ItemTypeBar
-
-export class ItemTypesBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showAddItemTypesModal: false,
-    }
-  }
-
-  showAddItemTypesModal = () => {
-    this.setState({
-      showAddItemTypesModal: true,
-    });
-  }
-
-  hideAddItemTypesModal = () => {
-    this.setState({
-      showAddItemTypesModal: false,
-    });
-  }
-
-  handleOkAddItemTypesModal = (e) => {
-    this.setState({
-      showAddItemTypesModal: false,
-    });
-  }
-
-  handleCancelAddItemTypesModal = (e) => {
-    this.setState({
-      showAddItemTypesModal: false,
-    });
-  }
-
-  render() {
-    return (
-      <div style={{ display: 'flex', flexDirection: 'row', margin: 15, justifyContent: 'flex-end' }}>
-        <Button onClick={this.showAddItemTypesModal}>
-          <Icon type="plus-circle" theme='filled' style={{ color: '#1890FF' }} />
-          Add Item Type
-        </Button>
-        {this.state.showAddItemTypesModal && <AddItemTypesModal handleCancelAddItemTypesModal={this.handleCancelAddItemTypesModal} hide={this.hideAddItemTypesModal} accessToken={this.props.accessToken} />}
-      </div>
-    )
-  }
-}
-// #endregion
 
 //#region UserBar
 export class UserBar extends Component {
@@ -90,12 +43,171 @@ export class UserBar extends Component {
 
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'row', margin: 15, justifyContent: 'flex-end' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', margin: 15, marginBottom: 5, justifyContent: 'flex-end' }}>
+      <div style={{ flex: 1, justifyContent: 'flex-start' }}> 
+          <h2>Users</h2> 
+        </div>
         <Button onClick={this.showAddUserModal}>
           <Icon type="plus-circle" theme='filled' style={{ color: '#1890FF' }} />
           Add User
         </Button>
         {this.state.showUserModal && <AddUserModal addUser={this.props.addUser} handleCancelUserModal={this.handleCancelUserModal} hide={this.hideAddUserModal} />}
+      </div>
+    )
+  }
+}
+// #endregion
+
+//#region UserGroupBar
+export class UserGroupBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showUserGroupModal: false,
+      invalidUserGroup: false,
+    }
+  }
+  showAddUserGroupModal = () => {
+    this.setState({
+      showUserGroupModal: true,
+      invalidUserGroup: false,
+    });
+  }
+
+  hideAddUserGroupModal = () => {
+    this.setState({
+      showUserGroupModal: false,
+      invalidUserGroup: false,
+    });
+  }
+
+  handleOkModal = (e) => {
+    this.setState({
+      showUserGroupModal: false,
+    });
+  }
+
+  handleCancelUserGroupModal = (e) => {
+    this.setState({
+      showUserGroupModal: false,
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row', margin: 15, marginBottom: 5, justifyContent: 'flex-end' }}>
+      <div style={{ flex: 1, justifyContent: 'flex-start' }}> 
+          <h2>User Groups</h2> 
+        </div>
+        <Button onClick={this.showAddUserGroupModal}>
+          <Icon type="plus-circle" theme='filled' style={{ color: '#1890FF' }} />
+          Add User Group
+        </Button>
+        {/* {this.state.showUserGroupModal && <AddUserGroupModal addUserGroup={this.props.addUserGroup} handleCancelUserGroupModal={this.handleCancelUserGroupModal} hide={this.hideAddUserGroupModal} />} */}
+      </div>
+    )
+  }
+}
+// #endregion
+
+//#region PermissionBar
+export class PermissionBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showPermissionModal: false,
+      invalidPermission: false,
+    }
+  }
+  showAddPermissionModal = () => {
+    this.setState({
+      showPermissionModal: true,
+      invalidPermission: false,
+    });
+  }
+
+  hideAddPermissionModal = () => {
+    this.setState({
+      showPermissionModal: false,
+      invalidPermission: false,
+    });
+  }
+
+  handleOkModal = (e) => {
+    this.setState({
+      showPermissionModal: false,
+    });
+  }
+
+  handleCancelPermissionModal = (e) => {
+    this.setState({
+      showPermissionModal: false,
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row', margin: 15, marginBottom: 5, justifyContent: 'flex-end' }}>
+      <div style={{ flex: 1, justifyContent: 'flex-start' }}> 
+          <h2>Permissions</h2> 
+        </div>
+        <Button onClick={this.showAddPermissionModal}>
+          <Icon type="plus-circle" theme='filled' style={{ color: '#1890FF' }} />
+          Add Permission
+        </Button>
+        {/* {this.state.showPermissionModal && <AddPermissionModal addPermission={this.props.addPermission} handleCancelPermissionModal={this.handleCancelPermissionModal} hide={this.hideAddPermissionModal} />} */}
+      </div>
+    )
+  }
+}
+// #endregion
+
+//#region ItemTypeBar
+export class ItemTypesBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAddItemTypesModal: false,
+    }
+  }
+
+  showAddItemTypesModal = () => {
+    this.setState({
+      showAddItemTypesModal: true,
+    });
+  }
+
+  hideAddItemTypesModal = () => {
+    this.setState({
+      showAddItemTypesModal: false,
+    });
+  }
+
+  handleOkAddItemTypesModal = (e) => {
+    this.setState({
+      showAddItemTypesModal: false,
+    });
+  }
+
+  handleCancelAddItemTypesModal = (e) => {
+    this.setState({
+      showAddItemTypesModal: false,
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row', margin: 15, marginBottom: 5, justifyContent: 'flex-end' }}>
+        <div style={{ flex: 1, justifyContent: 'flex-start' }}> 
+          <h2>Item Types</h2> 
+        </div>
+        <Button onClick={this.showAddItemTypesModal}>
+          <Icon type="plus-circle" theme='filled' style={{ color: '#1890FF' }} />
+          Add Item Type
+        </Button>
+        {this.state.showAddItemTypesModal && <AddItemTypesModal handleCancelAddItemTypesModal={this.handleCancelAddItemTypesModal} hide={this.hideAddItemTypesModal} accessToken={this.props.accessToken} />}
       </div>
     )
   }
@@ -137,7 +249,10 @@ export class ManageProjectBar extends Component {
 
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'row', margin: 15, justifyContent: 'flex-end' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', margin: 15, marginBottom: 5, justifyContent: 'flex-end' }}>
+      <div style={{ flex: 1, justifyContent: 'flex-start' }}> 
+          <h2>Projects</h2> 
+        </div>
         <Button onClick={this.showAddProjectModal}>
           <Icon type="plus-circle" theme='filled' style={{ color: '#1890FF' }} />
           Add Project
