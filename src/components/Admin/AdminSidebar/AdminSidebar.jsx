1ace38309cabbba0aa0fa916d9bd2b10f7ba51fe
Owen Vey <owen.vey@gmail.com>
2019-03-11 18:15:31 -0500

    turned AdminSidebar into functional component

--- src/components/Admin/AdminSidebar/AdminSidebar.jsx
+++ src/components/Admin/AdminSidebar/AdminSidebar.jsx
@@ -1,56 +1,48 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Menu, Icon } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import './AdminSidebar.css'
 
-class AdminSidebar extends Component {
+const AdminSidebar = (props) => {
 
-  handleItemClick = (item) => {
-    this.props.handlePageChange(item.key);
-  }
+  return (
+    <Menu
+      defaultSelectedKeys={['organizationDetails']}
+      mode='vertical'
+      onSelect={(item) => props.handlePageChange(item.key)}
+    >
+      <Menu.Item key="organizationDetails">
+        <Icon><FontAwesomeIcon icon='building' /></Icon>
+        <span>Organization Details</span>
+      </Menu.Item>
 
-  render() {
-    return (
-      <Menu
-        defaultSelectedKeys={['organizationDetails']}
-        mode='vertical'
-        onSelect={this.handleItemClick}
-      >
-        <Menu.Item key="organizationDetails">
-          <Icon><FontAwesomeIcon icon='building' /></Icon>
-          <span>Organization Details</span>
-        </Menu.Item>
+      <Menu.Item key="users">
+        <Icon><FontAwesomeIcon icon='user' /></Icon>
+        <span>Users</span>
+      </Menu.Item>
 
-        <Menu.Item key="users">
-          <Icon><FontAwesomeIcon icon='user' /></Icon>
-          <span>Users</span>
-        </Menu.Item>
+      <Menu.Item key="userGroups">
+        <Icon>  <FontAwesomeIcon icon='users' /></Icon>
+        <span>User Groups</span>
+      </Menu.Item>
 
-        <Menu.Item key="userGroups">
-          <Icon>  <FontAwesomeIcon icon='users' /></Icon>
-          <span>User Groups</span>
-        </Menu.Item>
+      <Menu.Item key="permissions">
+        <Icon><FontAwesomeIcon icon='shield-alt' /></Icon>
+        <span>Permissions</span>
+      </Menu.Item>
 
-        <Menu.Item key="permissions">
-          <Icon><FontAwesomeIcon icon='shield-alt' /></Icon>
-          <span>Permissions</span>
-        </Menu.Item>
+      <Menu.Item key="itemTypes">
+        <Icon><FontAwesomeIcon icon='list' /></Icon>
+        <span>Item Types</span>
+      </Menu.Item>
 
-        <Menu.Item key="itemTypes">
-          <Icon><FontAwesomeIcon icon='list' /></Icon>
-          <span>Item Types</span>
-        </Menu.Item>
+      <Menu.Item key="manageAllProjects">
+        <Icon><FontAwesomeIcon icon='cogs' /></Icon>
+        <span>Manage All Projects</span>
+      </Menu.Item>
 
-        <Menu.Item key="manageAllProjects">
-          <Icon><FontAwesomeIcon icon='cogs' /></Icon>
-          <span>Manage All Projects</span>
-        </Menu.Item>
-
-      </Menu>
-    );
-  }
+    </Menu>
+  );
 }
-
 
 export default AdminSidebar;
