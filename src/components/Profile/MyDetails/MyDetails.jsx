399e2c4cf36b3504f34de73397292ee7758074d9
Mo <mthao09.mt@gmail.com>
2019-03-26 19:28:04 -0500

    fix bugs

--- src/components/Profile/MyDetails/MyDetails.jsx
+++ src/components/Profile/MyDetails/MyDetails.jsx
@@ -4,15 +4,7 @@ import { connect } from "react-redux";
 const MyDetails = ({ loginUser }) => {
   return (
     <>
-      <div style={{ display: 'flex', flexDirection: 'row', margin: 15, marginBottom: 5, justifyContent: 'flex-end' }}>
-        <div style={{ flex: 1, justifyContent: 'flex-start' }}>
-          <h2>Item Types</h2>
-        </div>
-        <Button onClick={() => this.props.clickAddItemType()}>
-          <Icon type="plus-circle" theme='filled' style={{ color: '#1890FF' }} />
-          Edit Profile
-          </Button>
-      </div>
+
       <div><strong>First Name:</strong> {loginUser.firstName}</div>
       <div><strong>Last Name:</strong> {loginUser.lastName}</div>
       <div><strong>Email:</strong> {loginUser.email}</div>
