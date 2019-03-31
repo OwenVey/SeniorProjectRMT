cc340ac60b5ce3583b9f3be18c2437432531881c
Alex Tilot <tilota@uwplatt.edu>
2019-03-31 13:56:28 -0500

    updated styling

--- src/components/NoPermissionToAccess/NoPermissionToAccess.jsx
+++ src/components/NoPermissionToAccess/NoPermissionToAccess.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { connect } from "react-redux";
 
 
 class NoPermissionToAccess extends Component {
     render() {
         return (
-            <div className='centered'>
-                {!this.props.adminAccess && <h1 >You do not have sufficient permissions to view this page</h1>}
-            </div>
+            <React.Fragment>
+                {!this.props.adminAccess && <div className='centered' style={{ display: 'flex', flexDirection: 'row', margin: 15, marginBottom: 5,  border: '#f5f9fa' }}>
+                    <div>
+                        <FontAwesomeIcon size='9x' color='#eaec38' icon='exclamation-triangle' />
+                    </div>
+                    <div style ={{ marginLeft: 15}}>
+                        <h1 >Error</h1>
+                        <p>You do not have sufficient permissions to view this page</p>
+                    </div>
+                </div>}
+            </React.Fragment>
         );
     }
 }
