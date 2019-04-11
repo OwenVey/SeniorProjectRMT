3c31701b48cad198f050e41a9ee5c3533a328075
Alex Tilot <tilota@uwplatt.edu>
2019-04-11 18:26:44 -0500

    added userGroups Testing

---
+++ src/actions/mocks/userGroupMocks.js
@@ -0,0 +1,54 @@
+export const getUserGroupsMock = {
+    groups: [
+        {
+            id: 0,
+            projectId: 0,
+            name: "Big Banana",
+            description: "Banana Club"
+        },
+        {
+            id: 1,
+            projectId: 0,
+            name: "Big Apple",
+            description: "We love apples"
+        },
+        {
+            id: 2,
+            projectId: 0,
+            name: "small Hog",
+            description: "Where the pigs hang out"
+        },
+        {
+            id: 3,
+            projectId: 0,
+            name: "Huge Turtle",
+            description: "Slow but we will catch you"
+        },
+        {
+            id: 4,
+            projectId: 0,
+            name: "The Chet Crew",
+            description: "Bunch of Crazy boies"
+        },
+        {
+            id: 5,
+            projectId: 0,
+            name: "Alex and the big shots",
+            description: "WE think we are cool"
+        }
+    ]
+}
+
+export const addUserGroupsMock = {
+    "id": 6,
+    "projectId": 1,
+    "name": "Falcon Caaw",
+    "description": "We fly high and can strike"
+}
+
+export const editUserGroupsMock = {
+    "id": 0,
+    "projectId": 0,
+    "name": "Snall Banana",
+    "description": "New Banana Club"
+}
